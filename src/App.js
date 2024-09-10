d7ad6b5b583418dcfd4823ab4d38c0b8386275c1
kohey94 <sugawara622@gmail.com>
2024-09-11 02:26:50 +0900

    マップ表示

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+require('dotenv').config();
+
+const containerStyle = {
+  width: '100%',
+  height: '1000px'
+};
+
+const center = {
+  lat: 35.6895, // Tokyo
+  lng: 139.6917
+};
 
 function App() {
+  const [googleMapsApiKey, setGoogleMapsApiKey] = useState('');
+  
+  useEffect(() => {
+    setGoogleMapsApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+  }, []);
+  
+  if (!googleMapsApiKey) {
+    // Render a loading indicator or fallback UI until the API key is available
+    return <div>Loading...</div>;
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <LoadScript googleMapsApiKey={googleMapsApiKey}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={15}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
 export default App;
